637c0c98ac3fbc86d69c707995976fdced68f28b
dailysheen <saun452@gmail.com>
2026-07-24 00:45:33 +0600

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration object with necessary keys.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
+// Daily Sheen - Firebase Configuration
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDAjVou0DY_7hCgNpTziDEUfK7OhfbC3E",
+  authDomain: "daily-sheen.firebaseapp.com",
+  projectId: "daily-sheen",
+  storageBucket: "daily-sheen.firebasestorage.app",
+  messagingSenderId: "521433909326",
+  appId: "1:521433909326:web:70e43e713f21b93e6ed1b1f"
+};
