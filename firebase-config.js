23d89f38fc851e770a61cc0ddbbecc1e44a94533
dailysheen <saun452@gmail.com>
2026-07-24 01:23:36 +0600

    Update Firebase configuration and API key
    Updated Firebase configuration with new API key and version.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,27 @@
-// Daily Sheen - Firebase Configuration
+// ==========================================
+// Daily Sheen V7
+// Firebase Configuration
+// ==========================================
 
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAjVou0DY_7hCgNpTziDEUfK7OhfbC3E",
+  apiKey: "AIzaSyDAjVouODY_7hCgNpTZiDEUKfT7ohfbCR0",
   authDomain: "daily-sheen.firebaseapp.com",
   projectId: "daily-sheen",
   storageBucket: "daily-sheen.firebasestorage.app",
   messagingSenderId: "521433909326",
-  appId: "1:521433909326:web:70e43e713f21b93e6ed1b1f"
+  appId: "1:521433909326:web:70e43e713f21b93e6d1b1f"
 };
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Export Firebase App
+export { app };
