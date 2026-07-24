30ef1325c4a0148ad24eed3237b0762a9ecede72
dailysheen <saun452@gmail.com>
2026-07-24 22:42:26 +0600

    Refactor Firebase configuration for readability

--- firebase-config.js
+++ firebase-config.js
@@ -1,27 +1,59 @@
-// ==========================================
-// Daily Sheen V7
+// ======================================================
+// Daily Sheen
 // Firebase Configuration
-// ==========================================
+// ======================================================
+
+
+// ======================================================
+// Firebase App
+// ======================================================
 
 import {
   initializeApp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 
+// ======================================================
 // Firebase Configuration
+// ======================================================
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDAjVouODY_7hCgNpTZiDEUKfT7ohfbCR0",
-  authDomain: "daily-sheen.firebaseapp.com",
-  projectId: "daily-sheen",
-  storageBucket: "daily-sheen.firebasestorage.app",
-  messagingSenderId: "521433909326",
-  appId: "1:521433909326:web:70e43e713f21b93e6d1b1f"
+
+  apiKey:
+    "AIzaSyDAjVouODY_7hCgNpTZiDEUKfT7ohfbCR0",
+
+  authDomain:
+    "daily-sheen.firebaseapp.com",
+
+  projectId:
+    "daily-sheen",
+
+  storageBucket:
+    "daily-sheen.firebasestorage.app",
+
+  messagingSenderId:
+    "521433909326",
+
+  appId:
+    "1:521433909326:web:70e43e713f21b93e6d1b1f"
+
 };
 
 
+// ======================================================
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// ======================================================
+
+const app =
+  initializeApp(
+    firebaseConfig
+  );
 
 
+// ======================================================
 // Export Firebase App
-export { app };
+// ======================================================
+
+export {
+  app
+};
